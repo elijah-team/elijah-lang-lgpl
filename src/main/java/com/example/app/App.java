ed51f5005a4fb40d2183235388273200feade067
triple oh <oluoluolu+gl@gmail.com>
2019-02-24 08:18:38 -0500

    try to fix build, was building nothing

---
+++ src/main/java/com/example/app/App.java
@@ -0,0 +1,11 @@
+package com.example.app;
+
+import com.example.dep.Dep;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Dep.hello( "GitLab" );
+    }
+}
---
+++ src/main/java/tripleo/elijah/util/NotImplementedException.java
@@ -0,0 +1,13 @@
+package tripleo.elijah.util;
+
+public class NotImplementedException extends RuntimeException {
+public NotImplementedException() {
+	int y=2;
+}
+
+public static void raise() {
+	int y=2;
+	
+}
+}
+
