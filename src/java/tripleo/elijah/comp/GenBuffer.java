936ac29e32c9fa688619c8b9b46c70ee9638dc1b
tripleo acer <oluoluolu+elijah@gmail.com>
2019-04-11 05:20:34 -0700

    added NotImplementedExceptions

--- src/java/tripleo/elijah/comp/GenBuffer.java
+++ src/java/tripleo/elijah/comp/GenBuffer.java
@@ -8,19 +8,42 @@
  */
 package tripleo.elijah.comp;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import tripleo.elijah.gen.CompilerContext;
 import tripleo.elijah.gen.nodes.ImportNode;
+import tripleo.elijah.util.NotImplementedException;
+import tripleo.util.buffer.Buffer;
+import tripleo.util.buffer.FileBackedBuffer;
 
 public class GenBuffer {
 
+	private Map<String, Buffer> hdr_bufs = new HashMap<String, Buffer>();
+
 	public void GenImportStmt(CompilerContext cctx, ImportNode impn) {
 		// TODO Auto-generated method stub
+		NotImplementedException.raise();
 		
 	}
 
 	public void InitMod(CompilerContext cctx, String string) {
 		// TODO Auto-generated method stub
+		NotImplementedException.raise();
 		
+	}
+
+	public Buffer moduleBufHdr(String module) {
+		// TODO Auto-generated method stub
+		NotImplementedException.raise();
+		Buffer buf = hdr_bufs.getOrDefault(module, new FileBackedBuffer(module+".h"));
+		return buf;
+	}
+
+	public Buffer moduleBufImpl(String module) {
+		// TODO Auto-generated method stub
+		NotImplementedException.raise();
+		return null;
 	}
 
 }
