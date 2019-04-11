b8e6ec6b95f8d1fb1278306ef80e02717363fdcd
tripleo acer <oluoluolu+elijah@gmail.com>
2019-04-11 05:23:10 -0700

    minor flesh

--- src/java/tripleo/elijah/gen/CompilerContext.java
+++ src/java/tripleo/elijah/gen/CompilerContext.java
@@ -1,5 +1,18 @@
 package tripleo.elijah.gen;
 
+import tripleo.elijah.util.NotImplementedException;
+
 public class CompilerContext {
+
+	private String _module;
+
+	public CompilerContext(String aModule) {
+		_module=aModule;
+	}
+	public String module() {
+		// TODO Auto-generated method stub
+		NotImplementedException.raise();
+		return _module;
+	}
 
 }
