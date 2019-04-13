a2515df3e1e05a406360b7beecb25b153a6e33ca
tripleo acer <oluoluolu+elijah@gmail.com>
2019-04-13 02:19:55 -0700

    CodeGen: implement appendHeader

--- buffer-src/java/tripleo/util/buffer/CodeGen.java
+++ buffer-src/java/tripleo/util/buffer/CodeGen.java
@@ -3,10 +3,43 @@
  */
 package tripleo.util.buffer;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import tripleo.elijah.util.NotImplementedException;
+
 /**
  * @author olu
  *
  */
 public class CodeGen {
+
+	public void appendHeader(String module, String build) {
+		// TODO Auto-generated method stub
+		NotImplementedException.raise();
+		FileOutputStream fileOutputStream = null;
+		try {
+			try {
+				fileOutputStream = new FileOutputStream(module + ".h");
+				fileOutputStream.write(build.toString().getBytes());
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+		} finally {
+			// TODO: handle finally clause
+			if (fileOutputStream != null)
+				try {
+					fileOutputStream.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}
+	}
 
 }
