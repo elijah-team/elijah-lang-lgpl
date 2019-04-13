4154ab4370d22024cd75c66453371eaf53ccda5b
tripleo acer <oluoluolu+elijah@gmail.com>
2019-04-13 02:20:38 -0700

    changed expr to ExpressinoNode and added getter and setter

--- src/java/tripleo/elijah/gen/nodes/CaseHdrNode.java
+++ src/java/tripleo/elijah/gen/nodes/CaseHdrNode.java
@@ -1,15 +1,22 @@
 package tripleo.elijah.gen.nodes;
 
-import tripleo.elijah.lang.OS_Expression;
 import tripleo.elijah.lang.VariableReference;
 
 public class CaseHdrNode {
 
-	private OS_Expression expr;
+	private ExpressionNode expr;
+
+	public ExpressionNode getExpr() {
+		return expr;
+	}
+
+	public void setExpr(ExpressionNode expr) {
+		this.expr = expr;
+	}
 
 	public CaseHdrNode(VariableReference varref) {
 		// TODO Auto-generated constructor stub
-		this.expr = varref;
+		this.expr = new ExpressionNode(varref);
 	}
 
 }
