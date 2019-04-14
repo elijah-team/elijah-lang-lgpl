f23272dc8cfe79f205c4fedeb9a6cf18c04b62df
tripleo acer <oluoluolu+elijah@gmail.com>
2019-04-13 22:50:31 -0700

    added scope

--- src/main/java/tripleo/elijah/lang/Loop.java
+++ src/main/java/tripleo/elijah/lang/Loop.java
@@ -9,8 +9,11 @@
 package tripleo.elijah.lang;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import tripleo.elijah.gen.ICodeGen;
+import tripleo.elijah.lang.FunctionDef.StatementWrapper;
 import tripleo.elijah.util.NotImplementedException;
 import tripleo.elijah.util.TabbedOutputStream;
 
@@ -18,6 +21,10 @@ import tripleo.elijah.util.TabbedOutputStream;
 //			Statement, LoopTypes, Scope
 
 public class Loop implements Statement, LoopTypes, StatementItem, FunctionItem {
+
+	private Scope _scope = new LoopScope();
+	private List<String> docstrings = new ArrayList<String>();
+	private List<StatementItem> items = new ArrayList<StatementItem>();
 
 	public Loop() {
 	}
@@ -27,7 +34,7 @@ public class Loop implements Statement, LoopTypes, StatementItem, FunctionItem {
 	}
 
 	public Scope scope() {
-		return null;
+		return _scope ;
 	}
 
 	public void expr(IExpression aExpr) {
@@ -48,15 +55,30 @@ public class Loop implements Statement, LoopTypes, StatementItem, FunctionItem {
 	}
 
 	String iterName;
+	/**
+	 * @category type
+	 */
 	int type;
 IExpression topart,expr; 
 IExpression frompart;
 
+	/**
+	 * @category type
+	 */
 	public final int FROM_TO_TYPE = 82;
 
+	/**
+	 * @category type
+	 */
 	public final int TO_TYPE = 81;
+	/**
+	 * @category type
+	 */
 	public final int ITER_TYPE = 86;
 
+	/**
+	 * @category type
+	 */
 	public final int EXPR_TYPE = 83;
 
 	@Override
@@ -70,4 +92,44 @@ IExpression frompart;
 		// TODO Auto-generated method stub
 		NotImplementedException.raise();
 	}
+
+	private final class LoopScope implements Scope {
+
+		private final AbstractStatementClosure asc = new AbstractStatementClosure(this);
+
+		@Override
+		public void add(StatementItem aItem) {
+//			if (aItem instanceof FunctionItem)
+//				items.add((FunctionItem) aItem);
+//			else
+//				System.err.println(String.format("adding false StatementItem %s",
+//					aItem.getClass().getName()));
+			items.add(aItem);
+		}
+
+		@Override
+		public void addDocString(String aS) {
+			docstrings.add(aS);
+		}
+
+		@Override
+		public BlockStatement blockStatement() {
+			return new BlockStatement(this);
+		}
+
+		@Override
+		public StatementClosure statementClosure() {
+			return asc;
+		}
+
+		@Override
+		public void statementWrapper(IExpression aExpr) {
+			add(new StatementWrapper(aExpr));
+			throw new NotImplementedException(); // TODO
+		}
+	}
 }
+
+//
+//
+//
