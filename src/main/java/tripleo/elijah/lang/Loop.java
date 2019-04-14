7f57d649ce1a7c3f04cae1ad9e1739ec74957c86
tripleo acer <oluoluolu+elijah@gmail.com>
2019-04-13 21:39:44 -0700

    remove add false Statementitem error

--- src/main/java/tripleo/elijah/lang/Loop.java
+++ src/main/java/tripleo/elijah/lang/Loop.java
@@ -8,11 +8,16 @@
  */
 package tripleo.elijah.lang;
 
+import java.io.IOException;
+
+import tripleo.elijah.gen.ICodeGen;
+import tripleo.elijah.util.NotImplementedException;
+import tripleo.elijah.util.TabbedOutputStream;
 
 // Referenced classes of package pak2:
 //			Statement, LoopTypes, Scope
 
-public class Loop implements Statement, LoopTypes, StatementItem {
+public class Loop implements Statement, LoopTypes, StatementItem, FunctionItem {
 
 	public Loop() {
 	}
@@ -53,4 +58,16 @@ IExpression frompart;
 	public final int ITER_TYPE = 86;
 
 	public final int EXPR_TYPE = 83;
+
+	@Override
+	public void print_osi(TabbedOutputStream aTos) throws IOException {
+		// TODO Auto-generated method stub
+		NotImplementedException.raise();
+	}
+
+	@Override
+	public void visitGen(ICodeGen visit) {
+		// TODO Auto-generated method stub
+		NotImplementedException.raise();
+	}
 }
