b2b60b1011465eec11cbb1a441ad6453757a125d
tripleo acer <oluoluolu+elijah@gmail.com>
2019-04-18 16:26:31 -0700

    added license

--- src/main/java/tripleo/elijah/ProgramClosure.java
+++ src/main/java/tripleo/elijah/ProgramClosure.java
@@ -1,3 +1,11 @@
+/*
+ * Elijjah compiler, copyright Tripleo <oluoluolu+elijah@gmail.com>
+ *
+ * The contents of this library are released under the LGPL licence v3,
+ * the GNU Lesser General Public License text was downloaded from
+ * http://www.gnu.org/licenses/lgpl.html from `Version 3, 29 June 2007'
+ *
+ */
 package tripleo.elijah;
 
 import tripleo.elijah.lang.ClassStatement;
@@ -18,9 +26,12 @@ public class ProgramClosure {
 	}
 	
 	public NamespaceStatement namespaceStatement(OS_Module aParent) {
-		// TODO Auto-generated method stub
 		final NamespaceStatement namespaceStatement = new NamespaceStatement(aParent);
 		return namespaceStatement;
 	}
 	
 }
+
+//
+//
+//
