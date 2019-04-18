ee2219219a320caa27c2cfcdc37bb3f4787a32ee
tripleo acer <oluoluolu+elijah@gmail.com>
2019-04-17 19:54:50 -0700

    added copyright

--- src/main/java/tripleo/elijah/gen/nodes/CaseHdrNode.java
+++ src/main/java/tripleo/elijah/gen/nodes/CaseHdrNode.java
@@ -1,3 +1,17 @@
+/*
+ * Elijjah compiler, copyright Tripleo <oluoluolu+elijah@gmail.com>
+ *
+ * The contents of this library are released under the LGPL licence v3,
+ * the GNU Lesser General Public License text was downloaded from
+ * http://www.gnu.org/licenses/lgpl.html from `Version 3, 29 June 2007'
+ *
+ */
+/*
+ * Created on Sep 2, 2005 2:28:42 PM
+ *
+ * $Id$
+ *
+ */
 package tripleo.elijah.gen.nodes;
 
 import tripleo.elijah.lang.VariableReference;
@@ -18,5 +32,9 @@ public class CaseHdrNode {
 		// TODO Auto-generated constructor stub
 		this.expr = new ExpressionNode(varref);
 	}
-
+	
 }
+
+//
+//
+//
