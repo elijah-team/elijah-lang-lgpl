b25b6c1dc7b08ddaccba9b002bd9ee9a4ee17a59
tripleo acer <oluoluolu+elijah@gmail.com>
2019-04-19 16:11:23 -0700

    add copyright and add tmp allocation to ctor

--- src/main/java/tripleo/elijah/gen/nodes/LocalAgnTmpNode.java
+++ src/main/java/tripleo/elijah/gen/nodes/LocalAgnTmpNode.java
@@ -1,4 +1,9 @@
-/**
+/*
+ * Elijjah compiler, copyright Tripleo <oluoluolu+elijah@gmail.com>
+ * 
+ * The contents of this library are released under the LGPL licence v3, 
+ * the GNU Lesser General Public License text was downloaded from
+ * http://www.gnu.org/licenses/lgpl.html from `Version 3, 29 June 2007'
  * 
  */
 package tripleo.elijah.gen.nodes;
@@ -11,16 +16,23 @@ import tripleo.elijah.lang.IExpression;
  */
 public class LocalAgnTmpNode {
 
-	private IExpression expr;
+//	private IExpression expr;
 	private TmpSSACtxNode agnTo;
 	private ExpressionNode agnWhat;
+	private int n;
+	private String declared = null;
 
-	public LocalAgnTmpNode(TmpSSACtxNode tccssan, IExpression binex) {
+	public LocalAgnTmpNode(TmpSSACtxNode tccssan, IExpression expression) {
 		// TODO Auto-generated constructor stub
 		this.agnTo = tccssan;
-		this.expr = binex;
+//		this.expr = expression;
 		//
-		setRight(new ExpressionNode(binex));
+		setRight(new ExpressionNode(expression));
+		//
+		agnTo.__expr = expression;
+		//
+		n=tccssan.getCtx().nextTmp();
+		this.declared ="vt"+n;
 	}
 	
 	public ExpressionNode getRight() {
@@ -30,4 +42,12 @@ public class LocalAgnTmpNode {
 	public void setRight(ExpressionNode agnWhat) {
 		this.agnWhat = agnWhat;
 	}
+	
+	public ExpressionNode getLeft() {
+		return agnWhat;
+	}
 }
+
+//
+//
+//
