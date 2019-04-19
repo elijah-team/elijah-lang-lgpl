b0254551b17b0880297f1cca615afebc884f91e1
tripleo acer <oluoluolu+elijah@gmail.com>
2019-04-19 15:56:42 -0700

    added temp allocation for tmpssactx

--- src/main/java/tripleo/elijah/gen/CompilerContext.java
+++ src/main/java/tripleo/elijah/gen/CompilerContext.java
@@ -1,10 +1,17 @@
+/*
+ * Elijjah compiler, copyright Tripleo <oluoluolu+elijah@gmail.com>
+ * 
+ * The contents of this library are released under the LGPL licence v3, 
+ * the GNU Lesser General Public License text was downloaded from
+ * http://www.gnu.org/licenses/lgpl.html from `Version 3, 29 June 2007'
+ * 
+ */
 package tripleo.elijah.gen;
-
-import tripleo.elijah.util.NotImplementedException;
 
 public class CompilerContext {
 
 	private String _module;
+	private int _tmp=0;
 
 	public CompilerContext(String aModule) {
 		_module=aModule;
@@ -13,6 +20,10 @@ public class CompilerContext {
 		// TODO Auto-generated method stub
 //		NotImplementedException.raise();
 		return _module;
+	}
+	public int nextTmp() {
+		// TODO Auto-generated method stub
+		return ++_tmp;
 	}
 
 }
