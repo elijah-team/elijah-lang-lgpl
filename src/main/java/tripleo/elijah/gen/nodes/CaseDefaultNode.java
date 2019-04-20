05c8257bdf81902651315ad266ed4672051cf8cf
tripleo acer <oluoluolu+elijah@gmail.com>
2019-04-20 11:55:28 -0700

    removed unneeded class

--- src/main/java/tripleo/elijah/gen/nodes/CaseDefaultNode.java
+++ src/main/java/tripleo/elijah/gen/nodes/CaseDefaultNode.java
@@ -1,23 +1,23 @@
-/**
- * Created Mar 13, 2019 at 11:07:07 AM
- *
- */
-package tripleo.elijah.gen.nodes;
-
-import tripleo.elijah.gen.CompilerContext;
-import tripleo.elijah.lang.VariableReference;
-
-/**
- * same as case choice node
- *
- * @author SBUSER
- *
- */
-public class CaseDefaultNode {
-
-	public CaseDefaultNode(CompilerContext cctx, VariableReference varref) {
-		// TODO Auto-generated constructor stub
-	}
-
-
-}
+///**
+// * Created Mar 13, 2019 at 11:07:07 AM
+// *
+// */
+//package tripleo.elijah.gen.nodes;
+//
+//import tripleo.elijah.gen.CompilerContext;
+//import tripleo.elijah.lang.VariableReference;
+//
+///**
+// * same as case choice node
+// *
+// * @author SBUSER
+// *
+// */
+//public class CaseDefaultNode {
+//
+//	public CaseDefaultNode(CompilerContext cctx, VariableReference varref) {
+//		// TODO Auto-generated constructor stub
+//	}
+//
+//
+//}
