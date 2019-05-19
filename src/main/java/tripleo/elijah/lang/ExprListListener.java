fab120c75011383f989c82c57a90cc44f694482b
tripleo acer <oluoluolu+elijah@gmail.com>
2019-05-19 03:03:29 -0400

    readdded. still not needed i dont think.

--- src/main/java/tripleo/elijah/lang/ExprListListener.java
+++ src/main/java/tripleo/elijah/lang/ExprListListener.java
@@ -1,21 +1,21 @@
-///*
-// * Elijjah compiler, copyright Tripleo <oluoluolu+elijah@gmail.com>
-// *
-// * The contents of this library are released under the LGPL licence v3,
-// * the GNU Lesser General Public License text was downloaded from
-// * http://www.gnu.org/licenses/lgpl.html from `Version 3, 29 June 2007'
-// *
-// */
-//package tripleo.elijah.lang;
-//
-//// Referenced classes of package pak:
-////			IExpression
-//
-//public interface ExprListListener {
-//
-//	public abstract void change(IExpression iexpression);
-//
-//	public abstract String repr_();
-//
-//	public abstract boolean isEmpty();
-//}
+/*
+ * Elijjah compiler, copyright Tripleo <oluoluolu+elijah@gmail.com>
+ *
+ * The contents of this library are released under the LGPL licence v3,
+ * the GNU Lesser General Public License text was downloaded from
+ * http://www.gnu.org/licenses/lgpl.html from `Version 3, 29 June 2007'
+ *
+ */
+package tripleo.elijah.lang;
+
+// Referenced classes of package pak:
+//			IExpression
+
+public interface ExprListListener {
+
+	void change(IExpression iexpression);
+
+	String repr_();
+
+	boolean isEmpty();
+}
