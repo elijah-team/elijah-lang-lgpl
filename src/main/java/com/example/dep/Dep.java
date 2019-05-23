a5666826447a87737bfb1894203c7e28357215f9
tripleo acer <oluoluolu+elijah@gmail.com>
2019-05-23 19:50:25 -0400

    just for the fuck of it https://gitlab.com/gitlab-examples/multi-project-pipelines/simple-maven-dep.git

---
+++ src/main/java/com/example/dep/Dep.java
@@ -0,0 +1,13 @@
+package com.example.dep;
+
+/**
+ * Hello name!
+ *
+ */
+public class Dep
+{
+    public static void hello( String name )
+    {
+        System.out.println( "Hello " + name + "!" );
+    }
+}
