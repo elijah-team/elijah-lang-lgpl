0990f45c6ee8344e2724fa5df07e4dd8d41a2fe9
Tripleo <oluoluolu+elijah@gmail.com>
2020-03-29 23:08:15 -0700

    added commons-cli to Compilation

--- src/main/java/tripleo/elijah/comp/Compilation.java
+++ src/main/java/tripleo/elijah/comp/Compilation.java
@@ -11,11 +11,17 @@ package tripleo.elijah.comp;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import antlr.ANTLRException;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
 import tripleo.elijah.Out;
 import tripleo.elijah.lang.OS_Element;
+import tripleo.elijah.lang.OS_Module;
 import tripleo.elijah.util.NotImplementedException;
 import tripleo.elijjah.ElijjahLexer;
 import tripleo.elijjah.ElijjahParser;
@@ -24,6 +30,7 @@ public class Compilation {
 
 	private IO io;
 	private ErrSink eee;
+	private List<OS_Module> modules = new ArrayList<OS_Module>();
 
 	public Compilation(ErrSink eee, IO io) {
 		// TODO Auto-generated constructor stub
@@ -50,19 +57,31 @@ public class Compilation {
 	//
 	//
 	//
-	
+
+	String stage = "O"; // Output
+
 	public void main(List<String> args, StdErrSink stdErrSink) {
 		StdErrSink errSink = stdErrSink;
 		try {
 			if (args.size() > 0) {
-				for (int i = 0; i < args.size(); i++)
-					if (args.get(i).equals("-showtree"))
-						showTree = true;
-					else if (args.get(i).charAt(0) == '-') {
-						NotImplementedException.raise();
-					} else {
-						doFile(new File(args.get(i)));
-					}
+				Options options = new Options();
+				options.addOption("s", true, "stage: E: parse; O: output");
+				options.addOption("showtree", false, "show tree");
+				CommandLineParser clp = new DefaultParser();
+				/*final*/ String[] args2 = args.toArray(new String[0]);
+				CommandLine cmd = clp.parse(options, args2);
+
+				if (cmd.hasOption("s")) {
+					stage = cmd.getOptionValue('s');
+				}
+				if (cmd.hasOption("showtree")) {
+					showTree = true;
+				}
+
+				args2 = cmd.getArgs();
+
+				for (int i = 0; i < args2.length; i++)
+					doFile(new File(args2[i]));
 			} else {
 				System.err.println("Usage: eljc [-showtree] <directory or file name>");
 			}
@@ -71,7 +90,7 @@ public class Compilation {
 		}
 	}
 
-	public static void doFile(File f) throws Exception {
+	public void doFile(File f) throws Exception {
 		final String EXTENSION = ".elijah";
 		
 		if (f.isDirectory()) {
@@ -92,12 +111,12 @@ public class Compilation {
 		}
 	}
 
-	public static void parseFile(String f, InputStream s) throws Exception {
+	public void parseFile(String f, InputStream s) throws Exception {
 		try {
 			ElijjahLexer lexer = new ElijjahLexer(s);
 			lexer.setFilename(f);
 			ElijjahParser parser = new ElijjahParser(lexer);
-			parser.out = new Out(f);
+			parser.out = new Out(f, this);
 			parser.setFilename(f);
 			parser.program();
 		} catch (ANTLRException e) {
@@ -110,7 +129,19 @@ public class Compilation {
 
 	public OS_Element findClass(String string) {
 		// TODO Auto-generated method stub
+		for (OS_Module module : modules) {
+			if (module.hasClass(string)) {
+				return module.findClass(string);
+			}
+		}
 		return null;
 	}
 
+	public void addModule(OS_Module module) {
+		modules.add(module);
+	}
 }
+
+//
+//
+//
