ad063c39fc58f721db55e76c386497c9c9443838
Tripleo <oluoluolu+elijah@gmail.com>
2020-03-30 03:42:37 -0700

    added getters and setters

--- src/main/java/tripleo/elijah/lang/LookupResult.java
+++ src/main/java/tripleo/elijah/lang/LookupResult.java
@@ -10,6 +10,48 @@ package tripleo.elijah.lang;
  */
 public class LookupResult {
 
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the element
+	 */
+	public OS_Element getElement() {
+		return element;
+	}
+
+	/**
+	 * @param element the element to set
+	 */
+	public void setElement(OS_Element element) {
+		this.element = element;
+	}
+
+	/**
+	 * @return the level
+	 */
+	public int getLevel() {
+		return level;
+	}
+
+	/**
+	 * @param level the level to set
+	 */
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
 	private String name;
 	private OS_Element element;
 	private int level;
