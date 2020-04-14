7c0f836bf17135b4ef8fd6d17a69e9cbdb7de09a
Tripleo envy <oluoluolu+elijah@gmail.com>
2020-04-13 21:17:27 -0700

    backup of test

---
+++ src/test/java/tripleo/elijah/stages/deduce/DeduceTypesTest2.java
@@ -0,0 +1,42 @@
+package tripleo.elijah.stages.deduce;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import tripleo.elijah.contexts.FunctionContext;
+import tripleo.elijah.gen.nodes.Helpers;
+import tripleo.elijah.lang.*;
+import tripleo.elijah.lang2.BuiltInTypes;
+
+public class DeduceTypesTest2 {
+
+	@Test
+	public void testDeduceIdentExpression() {
+		OS_Module mod = new OS_Module();
+		DeduceTypes d = new DeduceTypes(mod);
+		ClassStatement cs = new ClassStatement(mod);
+		cs.setName(Helpers.makeToken("Test"));
+		final FunctionDef fd = cs.funcDef();
+		fd.setName(Helpers.makeToken("test"));
+		VariableSequence vss = fd.scope().statementClosure().varSeq();
+		final VariableStatement vs = vss.next();
+		vs.setName(Helpers.makeToken("x"));
+		final Qualident qu = new Qualident();
+		qu.append(Helpers.makeToken("Integer"));
+		vs.typeName().setName(qu);
+		FunctionContext fc = (FunctionContext) fd.getContext(); // TODO needs to be mocked
+		OS_Type x = d.deduceExpression(new IdentExpression(Helpers.makeToken("x")), fc);
+		System.out.println(x);
+//		Assert.assertEquals(new OS_Type(BuiltInTypes.SystemInteger).getBType(), x.getBType());
+//		final RegularTypeName tn = new RegularTypeName();
+		final VariableTypeName tn = new VariableTypeName();
+		Qualident tnq = new Qualident();
+		tnq.append(Helpers.makeToken("Integer"));
+		tn.setName(tnq);
+//		Assert.assertEquals(new OS_Type(tn).getTypeName(), x.getTypeName());
+//		Assert.assertEquals(new OS_Type(tn), x); // TODO this fails even when true
+		Assert.assertEquals(new OS_Type(tn).toString(), x.toString());
+	}
+
+}
