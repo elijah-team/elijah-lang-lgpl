0d28071180e02f696ec99476ee5b61e8e20c451c
Tripleo envy <oluoluolu+elijah@gmail.com>
2020-04-15 05:05:38 -0400

    added IndexingStatement

---
+++ src/main/java/tripleo/elijjah/lang/IndexingStatement.java
@@ -0,0 +1,42 @@
+/*
+ * Elijjah compiler, copyright Tripleo <oluoluolu+elijah@gmail.com>
+ * 
+ * The contents of this library are released under the LGPL licence v3, 
+ * the GNU Lesser General Public License text was downloaded from
+ * http://www.gnu.org/licenses/lgpl.html from `Version 3, 29 June 2007'
+ * 
+ */
+package tripleo.elijjah.lang;
+
+import antlr.Token;
+import tripleo.elijah.lang.ExpressionList;
+import tripleo.elijah.lang.OS_Module;
+
+/**
+ * @author Tripleo
+ *
+ * Created 	Apr 15, 2020 at 4:59:21 AM
+ */
+public class IndexingStatement {
+
+	private Token name;
+	private ExpressionList exprs;
+	private OS_Module parent;
+
+	public IndexingStatement(OS_Module module) {
+		this.parent = module;
+	}
+
+	public void setName(Token i1) {
+		this.name = i1;
+	}
+
+	public void setExprs(ExpressionList el) {
+		this.exprs = el;
+	}
+
+}
+
+//
+//
+//
