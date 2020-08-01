99260ab5023253d903f3b7e10dbfd160f2c0a545
Tripleo envybox <oluoluolu@gmail.com>
2020-08-01 14:35:11 -0400

    add copyleft

--- src/main/java/tripleo/elijah/lang/LookupResultList.java
+++ src/main/java/tripleo/elijah/lang/LookupResultList.java
@@ -1,3 +1,11 @@
+/*
+ * Elijjah compiler, copyright Tripleo <oluoluolu+elijah@gmail.com>
+ *
+ * The contents of this library are released under the LGPL licence v3,
+ * the GNU Lesser General Public License text was downloaded from
+ * http://www.gnu.org/licenses/lgpl.html from `Version 3, 29 June 2007'
+ *
+ */
 /**
  * 
  */
@@ -35,3 +43,7 @@ public class LookupResultList {
 			throw new NotImplementedException();
 	}
 }
+
+//
+//
+//
