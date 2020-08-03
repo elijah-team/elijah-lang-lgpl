1b0a1d22e025abb721e543551d31ebb991b67783
Tripleo envybox <oluoluolu@gmail.com>
2020-08-03 04:46:44 -0400

    added comments, javadoc

--- src/main/java/tripleo/elijah/lang/OS_Element2.java
+++ src/main/java/tripleo/elijah/lang/OS_Element2.java
@@ -9,6 +9,17 @@ package tripleo.elijah.lang;
  * Created 	Mar 23, 2020 at 12:40:27 AM
  */
 public interface OS_Element2 {
+
+	/**
+	 * The name of the element
+	 * TODO Should this be a {@link IdentExpression}?
+	 *
+	 * @return a String
+	 */
 	String name();
 
 }
+
+//
+//
+//
