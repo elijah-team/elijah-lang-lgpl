101a308b0c1a5fe8ee2597111023d16f5215233c
Tripleo envybox <oluoluolu@gmail.com>
2020-08-03 04:46:23 -0400

    added OS_FuncType to represent the type of (curried) functions

---
+++ src/main/java/tripleo/elijah/lang/OS_FuncType.java
@@ -0,0 +1,35 @@
+/*
+ * Elijjah compiler, copyright Tripleo <oluoluolu+elijah@gmail.com>
+ *
+ * The contents of this library are released under the LGPL licence v3,
+ * the GNU Lesser General Public License text was downloaded from
+ * http://www.gnu.org/licenses/lgpl.html from `Version 3, 29 June 2007'
+ *
+ */
+
+/**
+ *
+ */
+package tripleo.elijah.lang;
+
+public class OS_FuncType extends OS_Type {
+	private final FunctionDef function_def;
+
+	public OS_FuncType(FunctionDef functionDef) {
+		super();
+		this.function_def = functionDef;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return String.format("<OS_FuncType %s %s %s %s>", function_def);
+	}
+
+}
+
+//
+//
+//
