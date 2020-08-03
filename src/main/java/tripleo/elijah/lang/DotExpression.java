aff0be661d205c577d6c40361f9d05e150989c66
Tripleo envybox <oluoluolu@gmail.com>
2020-08-03 06:05:00 -0400

    cleanup

--- src/main/java/tripleo/elijah/lang/DotExpression.java
+++ src/main/java/tripleo/elijah/lang/DotExpression.java
@@ -16,9 +16,9 @@ public class DotExpression extends BasicBinaryExpression {
 		kind = ExpressionKind.DOT_EXP;
 	}
 
-	public DotExpression(IExpression ee, IExpression identExpression) {
+	public DotExpression(IExpression ee, IExpression aExpression) {
 		left = ee;
-		right = identExpression;
+		right = aExpression;
 		kind = ExpressionKind.DOT_EXP;
 	}
 
@@ -27,71 +27,8 @@ public class DotExpression extends BasicBinaryExpression {
 		return String.format("%s.%s", left, right);
 	}
 
-	//
-	// all this garbage below
-	//
-	
-//	/* (non-Javadoc)
-//	 * @see tripleo.elijah.lang.IExpression#print_osi(tripleo.elijah.util.TabbedOutputStream)
-//	 */
-//	@Override
-//	public void print_osi(TabbedOutputStream tabbedoutputstream) throws IOException {
-//		// TODO Auto-generated method stub
-//
-//	}
-//
-//	/* (non-Javadoc)
-//	 * @see tripleo.elijah.lang.IExpression#getType()
-//	 */
-//	@Override
-//	public ExpressionType getType() {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	/* (non-Javadoc)
-//	 * @see tripleo.elijah.lang.IExpression#set(tripleo.elijah.lang.ExpressionType)
-//	 */
-//	@Override
-//	public void set(ExpressionType aIncrement) {
-//		// TODO Auto-generated method stub
-//
-//	}
-//
-//	/* (non-Javadoc)
-//	 * @see tripleo.elijah.lang.IExpression#getLeft()
-//	 */
-//	@Override
-//	public IExpression getLeft() {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	/* (non-Javadoc)
-//	 * @see tripleo.elijah.lang.IExpression#setLeft(tripleo.elijah.lang.IExpression)
-//	 */
-//	@Override
-//	public void setLeft(IExpression iexpression) {
-//		// TODO Auto-generated method stub
-//
-//	}
-//
-//	/* (non-Javadoc)
-//	 * @see tripleo.elijah.lang.IExpression#repr_()
-//	 */
-//	@Override
-//	public String repr_() {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	/* (non-Javadoc)
-//	 * @see tripleo.elijah.lang.IExpression#is_simple()
-//	 */
-//	@Override
-//	public boolean is_simple() {
-//		// TODO Auto-generated method stub
-//		return false;
-//	}
-
 }
+
+//
+//
+//
