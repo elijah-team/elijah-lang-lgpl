4834653e4bf6ff6218c020ded1cda7b1f845aa15
Tripleo envybox <oluoluolu@gmail.com>
2020-09-07 11:18:03 -0400

    Begin to flesh out Java Translator

--- src/main/java/tripleo/elijah/stages/translate/TranslateModule.java
+++ src/main/java/tripleo/elijah/stages/translate/TranslateModule.java
@@ -1,7 +1,6 @@
 package tripleo.elijah.stages.translate;
 
-import tripleo.elijah.lang.ModuleItem;
-import tripleo.elijah.lang.OS_Module;
+import tripleo.elijah.lang.*;
 import tripleo.elijah.util.TabbedOutputStream;
 
 import java.io.IOException;
@@ -24,7 +23,24 @@ public class TranslateModule {
 			w = getFile();
 
 			for (ModuleItem item : module.getItems()) {
-				System.out.println("8000 "+item);
+				try {
+					if (item instanceof ClassStatement) {
+						w.put_string_ln("class C"+((ClassStatement) item).name()+ "{");
+						w.incr_tabs();
+						put_class_statement((ClassStatement) item, w);
+						w.dec_tabs();
+						w.put_string_ln("}");
+					} else if (item instanceof NamespaceStatement) {
+						w.put_string_ln("class NS_"+((NamespaceStatement) item).name()+ "{");
+						w.incr_tabs();
+						put_namespace_statement((NamespaceStatement) item, w);
+						w.dec_tabs();
+						w.put_string_ln("}");
+					} else
+						System.out.println("8000 "+item);
+				} catch (IOException e) {
+					module.parent.eee.exception(e);
+				}
 			}
 		} finally {
 			if (w != null) {
@@ -34,6 +50,40 @@ public class TranslateModule {
 					module.parent.eee.exception(e);
 				}
 			}
+		}
+	}
+
+	private void put_class_statement(ClassStatement classStatement, TabbedOutputStream w) throws IOException {
+		for (ClassItem item : classStatement.getItems()) {
+			if (item instanceof FunctionDef) {
+				w.put_string("public void "+((FunctionDef) item).name()+"(");
+				put_formal_arg_list(((FunctionDef) item).fal(), w);
+				w.put_string_ln(") {");
+				put_function_def((FunctionDef)item, w);
+				w.put_string_ln("}");
+			} else
+				System.out.println("8001 "+item);
+		}
+	}
+
+	private void put_formal_arg_list(FormalArgList fal, TabbedOutputStream w) throws IOException {
+		for (FormalArgListItem fali : fal.falis) {
+			w.put_string(fali.typeName().toString());
+			w.put_string(" ");
+			w.put_string(fali.name());
+			w.put_string(",");
+		}
+	}
+
+	private void put_function_def(FunctionDef functionDef, TabbedOutputStream w) {
+		for (FunctionItem item : functionDef.getItems()) {
+			System.out.println("8003 "+item);
+		}
+	}
+
+	private void put_namespace_statement(NamespaceStatement namespaceStatement, TabbedOutputStream w) {
+		for (ClassItem item : namespaceStatement.getItems()) {
+			System.out.println("8002 "+item);
 		}
 	}
 
