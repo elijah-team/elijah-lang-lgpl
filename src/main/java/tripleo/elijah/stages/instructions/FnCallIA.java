1784c153bca628db1f36eb71b319247918747fb4
Tripleo envybox <oluoluolu+elijah@gmail.com>
2020-09-13 03:45:14 -0400

    add args and constructor

--- src/main/java/tripleo/elijah/stages/instructions/FnCallIA.java
+++ src/main/java/tripleo/elijah/stages/instructions/FnCallIA.java
@@ -8,21 +8,31 @@
  */
 package tripleo.elijah.stages.instructions;
 
+import java.util.List;
+
 /**
  * Created 9/10/20 3:36 PM
  */
 public class FnCallIA implements InstructionArgument {
     private final Instruction expression_to_call;
+    private final List<InstructionArgument> args;
+
+    public FnCallIA(Instruction expression_to_call, List<InstructionArgument> args) {
+        this.expression_to_call = expression_to_call;
+        this.args = args;
+    }
 
     @Override
     public String toString() {
         return "FnCallIA{" +
-                "expression_to_call=" + expression_to_call.args +
+                "expression_to_call=" + expression_to_call +
+                ", args=" + args +
                 '}';
     }
 
     public FnCallIA(Instruction expression_to_call) {
         this.expression_to_call = expression_to_call;
+        this.args = null;
     }
 }
 
