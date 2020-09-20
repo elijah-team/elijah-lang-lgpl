b6ee27b76407efd839edf8a69dd923a254823416
Tripleo envybox <oluoluolu+elijah@gmail.com>
2020-09-20 00:46:31 -0400

    Add copyleft

--- src/main/java/tripleo/elijah/Documentable.java
+++ src/main/java/tripleo/elijah/Documentable.java
@@ -1,5 +1,10 @@
-/**
- * 
+/*
+ * Elijjah compiler, copyright Tripleo <oluoluolu+elijah@gmail.com>
+ *
+ * The contents of this library are released under the LGPL licence v3,
+ * the GNU Lesser General Public License text was downloaded from
+ * http://www.gnu.org/licenses/lgpl.html from `Version 3, 29 June 2007'
+ *
  */
 package tripleo.elijah;
 
@@ -15,3 +20,7 @@ public interface Documentable {
 	void addDocString(Token s1);
 
 }
+
+//
+//
+//
