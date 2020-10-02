4add4ef25dd9de1f5b223517a798300e22c23c0c
Tripleo envybox <oluoluolu+elijah@gmail.com>
2020-10-02 16:33:06 -0400

    add IdentIA

---
+++ src/main/java/tripleo/elijah/stages/instructions/IdentIA.java
@@ -0,0 +1,21 @@
+/*
+ * Elijjah compiler, copyright Tripleo <oluoluolu+elijah@gmail.com>
+ *
+ * The contents of this library are released under the LGPL licence v3,
+ * the GNU Lesser General Public License text was downloaded from
+ * http://www.gnu.org/licenses/lgpl.html from `Version 3, 29 June 2007'
+ *
+ */
+package tripleo.elijah.stages.instructions;
+
+/**
+ * Created 10/2/20 2:36 PM
+ */
+public class IdentIA implements InstructionArgument {
+	public IdentIA(int x) {
+	}
+}
+
+//
+//
+//
