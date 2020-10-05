fea050b2a9c2044bc1955a2cbcaefd1ad58c4f74
Tripleo envybox <oluoluolu+elijah@gmail.com>
2020-10-05 02:29:57 -0400

    add potentialTypes and type to IdentTableEntry

--- src/main/java/tripleo/elijah/stages/gen_fn/IdentTableEntry.java
+++ src/main/java/tripleo/elijah/stages/gen_fn/IdentTableEntry.java
@@ -11,6 +11,10 @@ package tripleo.elijah.stages.gen_fn;
 import tripleo.elijah.lang.IdentExpression;
 import tripleo.elijah.stages.instructions.InstructionArgument;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created 9/12/20 10:27 PM
  */
@@ -22,11 +26,31 @@ public class IdentTableEntry {
 	 * or a {@link tripleo.elijah.stages.instructions.IdentIA} which is an idte
 	 */
 	public InstructionArgument backlink;
+	public Map<Integer, TypeTableEntry> potentialTypes = new HashMap<Integer, TypeTableEntry>();
+	public TypeTableEntry type;
 
 	public IdentTableEntry(int index, IdentExpression ident) {
         this.index = index;
         this.ident = ident;
     }
+
+	public void addPotentialType(int instructionIndex, TypeTableEntry tte) {
+		potentialTypes.put(instructionIndex, tte);
+	}
+
+	public Collection<TypeTableEntry> potentialTypes() {
+		return potentialTypes.values();
+	}
+
+	@Override
+	public String toString() {
+		return "IdentTableEntry{" +
+				"index=" + index +
+				", ident=" + ident +
+				", backlink=" + backlink +
+				", potentialTypes=" + potentialTypes +
+				'}';
+	}
 }
 
 //
