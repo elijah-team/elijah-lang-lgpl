25ff214f86a4ec66488df7a0de88b5ddf0b4db0c
Tripleo envybox <oluoluolu+elijah@gmail.com>
2020-10-28 10:02:20 -0400

    move DeduceTypes

--- src/test/java/tripleo/elijah/stages/deduce/DeduceTypesTest2.java
+++ src/test/java/tripleo/elijah/stages/deduce/DeduceTypesTest2.java
@@ -25,7 +25,6 @@ public class DeduceTypesTest2 {
 	public void testDeduceIdentExpression() {
 		final OS_Module mod = new OS_Module();
 		mod.parent = new Compilation(new StdErrSink(), new IO());
-		final DeduceTypes d = new DeduceTypes(mod);
 		final ModuleContext mctx = new ModuleContext(mod);
 		mod.setContext(mctx);
 		final ClassStatement cs = new ClassStatement(mod);
@@ -33,18 +32,19 @@ public class DeduceTypesTest2 {
 		final ClassContext cctx = new ClassContext(mctx, cs);
 		cs.setContext(cctx);
 		final FunctionDef fd = cs.funcDef();
-		fd.setName(new IdentExpression(Helpers.makeToken("test")));
+		fd.setName((Helpers.string_to_ident("test")));
 		final FunctionContext fctx = new FunctionContext(cctx, fd);
 		fd.setContext(fctx);
 		final VariableSequence vss = fd.scope().statementClosure().varSeq(fctx);
 		final VariableStatement vs = vss.next();
-		vs.setName(new IdentExpression(tripleo.elijah.util.Helpers.makeToken("x")));
+		vs.setName((tripleo.elijah.util.Helpers.string_to_ident("x")));
 		final Qualident qu = new Qualident();
 		qu.append(tripleo.elijah.util.Helpers.makeToken("Integer"));
 		((NormalTypeName)vs.typeName()).setName(qu);
 		final FunctionContext fc = (FunctionContext) fd.getContext(); // TODO needs to be mocked
-		final IdentExpression x1 = new IdentExpression(tripleo.elijah.util.Helpers.makeToken("x"));
+		final IdentExpression x1 = (tripleo.elijah.util.Helpers.string_to_ident("x"));
 		x1.setContext(fc);
+		final DeduceTypes d = new DeduceTypes(mod);
 		final OS_Type x = d.deduceExpression(x1, fc);
 		System.out.println(x);
 //		Assert.assertEquals(new OS_Type(BuiltInTypes.SystemInteger).getBType(), x.getBType());
@@ -57,8 +57,8 @@ public class DeduceTypesTest2 {
 		cs.postConstruct();
 		mod.postConstruct();
 //		Assert.assertEquals(new OS_Type(tn).getTypeName(), x.getTypeName());
-//		Assert.assertEquals(new OS_Type(tn), x); // TODO this fails even when true
-		Assert.assertEquals(new OS_Type(tn).toString(), x.toString());
+		Assert.assertEquals(new OS_Type(tn), x);
+//		Assert.assertEquals(new OS_Type(tn).toString(), x.toString());
 	}
 
 }
