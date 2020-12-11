3186452eede5dbdbefc44e0c9a6ccf58de809630
Tripleo envybox <oluoluolu+elijah@gmail.com>
2020-12-11 13:45:25 -0500

    eh

--- src/test/java/tripleo/elijah/stages/deduce/DeduceTypesTest3.java
+++ src/test/java/tripleo/elijah/stages/deduce/DeduceTypesTest3.java
@@ -32,20 +32,20 @@ public class DeduceTypesTest3 {
 		final ModuleContext mctx = new ModuleContext(mod);
 		mod.setContext(mctx);
 		final ClassStatement cs = new ClassStatement(mod);
-		cs.setName(new IdentExpression(tripleo.elijah.util.Helpers.makeToken("Test")));
+		cs.setName(Helpers.string_to_ident("Test"));
 		final ClassContext cctx = new ClassContext(mctx, cs);
 		cs.setContext(cctx);
 		final ClassStatement cs_foo = new ClassStatement(mod);
-		cs_foo.setName(new IdentExpression(tripleo.elijah.util.Helpers.makeToken("Foo")));
+		cs_foo.setName(Helpers.string_to_ident("Foo"));
 		final ClassContext cctx_foo = new ClassContext(mctx, cs_foo);
 		cs_foo.setContext(cctx_foo);
 		final FunctionDef fd = cs.funcDef();
-		fd.setName(new IdentExpression(tripleo.elijah.util.Helpers.makeToken("test")));
+		fd.setName(Helpers.string_to_ident("test"));
 		final FunctionContext fctx = new FunctionContext(cctx, fd);
 		fd.setContext(fctx);
 		final VariableSequence vss = fd.scope().statementClosure().varSeq(fctx);
 		final VariableStatement vs = vss.next();
-		vs.setName(new IdentExpression(tripleo.elijah.util.Helpers.makeToken("x")));
+		vs.setName(Helpers.string_to_ident("x"));
 		final Qualident qu = new Qualident();
 		qu.append(Helpers.makeToken("Foo"));
 		((NormalTypeName)vs.typeName()).setName(qu);
@@ -55,14 +55,14 @@ public class DeduceTypesTest3 {
 		mod.postConstruct();
 		final FunctionContext fc = (FunctionContext) fd.getContext(); // TODO needs to be mocked
 		assert fc == fctx;
-		final IdentExpression x1 = new IdentExpression(tripleo.elijah.util.Helpers.makeToken("x"));
+		final IdentExpression x1 = Helpers.string_to_ident("x");
 		x1.setContext(fc);
 		this.x = d.deduceExpression(x1, fc);
 		System.out.println(this.x);
 	}
 
 //	/**
-//	 * Dont test BType here because its not a BUiltInType
+//	 * Don't test BType here because its not a BuiltInType
 //	 */
 //	@Test
 //	public void testDeduceIdentExpression1() {
