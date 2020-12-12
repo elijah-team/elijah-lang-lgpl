a027662a84a125f748c46d846027fc10a2e9a20a
Tripleo envybox <oluoluolu+elijah@gmail.com>
2020-12-12 12:12:05 -0500

    formatting:

--- src/main/java/tripleo/elijah/stages/gen_c/GenerateC.java
+++ src/main/java/tripleo/elijah/stages/gen_c/GenerateC.java
@@ -652,9 +652,10 @@ public class GenerateC {
 				sll.add(""+ const_to_string(constTableEntry.initialValue));
 			} else if (ia instanceof IntegerIA) {
 				final VariableTableEntry variableTableEntry = gf.getVarTableEntry(((IntegerIA) ia).getIndex());
-				sll.add("" + variableTableEntry.getName());
+				sll.add(variableTableEntry.getName());
 			} else if (ia instanceof IdentIA) {
-				@org.jetbrains.annotations.Nullable final OS_Element ident = gf.resolveIdentIA(gf.getFD().getContext(), (IdentIA) ia, module);
+				@org.jetbrains.annotations.Nullable
+				final OS_Element ident = gf.resolveIdentIA(gf.getFD().getContext(), (IdentIA) ia, module);
 				//String path = gf.getIAPath((IdentIA) ia));    // return x.y.z
 //				String path2 = gf.getIdentIAPath((IdentIA) ia); // return ZP105get_z(vvx.vmy)
 //				assert path.equals(path2); // should always fail
