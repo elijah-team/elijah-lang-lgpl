dc933f85f0a0e78d8acf985297c5a03aea0d9e42
Tripleo envybox <oluoluolu+elijah@gmail.com>
2020-12-23 07:50:18 -0500

    add Precondition and Postcondition

---
+++ src/main/java/tripleo/elijah/lang/Precondition.java
@@ -0,0 +1,29 @@
+/*
+ * Elijjah compiler, copyright Tripleo <oluoluolu+elijah@gmail.com>
+ *
+ * The contents of this library are released under the LGPL licence v3,
+ * the GNU Lesser General Public License text was downloaded from
+ * http://www.gnu.org/licenses/lgpl.html from `Version 3, 29 June 2007'
+ *
+ */
+package tripleo.elijah.lang;
+
+/**
+ * Created 12/22/20 11:44 PM
+ */
+public class Precondition {
+	private IdentExpression id;
+	private IExpression expr;
+
+	public void id(IdentExpression id) {
+		this.id = id;
+	}
+
+	public void expr(IExpression expr) {
+		this.expr = expr;
+	}
+}
+
+//
+//
+//
