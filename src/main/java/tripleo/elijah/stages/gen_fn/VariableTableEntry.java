03e59c3438b5cca8e049e8577524c99ae0440b5e
Tripleo envybox <oluoluolu+elijah@gmail.com>
2020-12-23 22:22:00 -0500

    avoid possible NPE

--- src/main/java/tripleo/elijah/stages/gen_fn/VariableTableEntry.java
+++ src/main/java/tripleo/elijah/stages/gen_fn/VariableTableEntry.java
@@ -59,7 +59,7 @@ public class VariableTableEntry {
 				//v.attached = v.attached; // leave it as is
 			} else if (tte.lifetime == v.lifetime && v.attached == tte.attached) {
 				// leave as is
-			} else if (tte.attached.equals(v.attached)) {
+			} else if (v.attached.equals(tte.attached)) {
 				// leave as is
 			} else
 				assert false;
