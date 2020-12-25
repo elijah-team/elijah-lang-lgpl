2fb59ccaf72b9fcaaab9e0902af9b5aa5d551de6
Tripleo envybox <oluoluolu+elijah@gmail.com>
2020-12-25 06:12:06 -0500

    fix comment

--- src/test/java/tripleo/elijah/stages/gen_fn/GetIdentIAPathTest.java
+++ src/test/java/tripleo/elijah/stages/gen_fn/GetIdentIAPathTest.java
@@ -1,3 +1,11 @@
+/*
+ * Elijjah compiler, copyright Tripleo <oluoluolu+elijah@gmail.com>
+ *
+ * The contents of this library are released under the LGPL licence v3,
+ * the GNU Lesser General Public License text was downloaded from
+ * http://www.gnu.org/licenses/lgpl.html from `Version 3, 29 June 2007'
+ *
+ */
 package tripleo.elijah.stages.gen_fn;
 
 import org.jetbrains.annotations.NotNull;
@@ -13,14 +21,6 @@ import tripleo.elijah.util.Helpers;
 
 import static org.easymock.EasyMock.*;
 
-/*
- * Elijjah compiler, copyright Tripleo <oluoluolu+elijah@gmail.com>
- *
- * The contents of this library are released under the LGPL licence v3,
- * the GNU Lesser General Public License text was downloaded from
- * http://www.gnu.org/licenses/lgpl.html from `Version 3, 29 June 2007'
- *
- */
 public class GetIdentIAPathTest {
 
 	GeneratedFunction gf;
