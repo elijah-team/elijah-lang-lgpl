b7b9813667e7fdd41e07677f995cc66959ce4189
Tripleo envybox <oluoluolu+elijah@gmail.com>
2020-12-27 16:26:20 -0500

    fix indentation

--- src/main/java/tripleo/elijah/stages/gen_fn/GenerateFunctions.java
+++ src/main/java/tripleo/elijah/stages/gen_fn/GenerateFunctions.java
@@ -816,17 +816,17 @@ public class GenerateFunctions {
 			}
 			return i;
 		case NUMERIC:
-		{
-			final NumericExpression ne = (NumericExpression) expression;
-			final int ii = addConstantTableEntry2(null, ne, ne.getType(), gf);
-			return new ConstTableIA(ii, gf);
-		}
+			{
+				final NumericExpression ne = (NumericExpression) expression;
+				final int ii = addConstantTableEntry2(null, ne, ne.getType(), gf);
+				return new ConstTableIA(ii, gf);
+			}
 		case CHAR_LITERAL:
-		{
-			final CharLitExpression cle = (CharLitExpression) expression;
-			final int ii = addConstantTableEntry2(null, cle, cle.getType(), gf);
-			return new ConstTableIA(ii, gf);
-		}
+			{
+				final CharLitExpression cle = (CharLitExpression) expression;
+				final int ii = addConstantTableEntry2(null, cle, cle.getType(), gf);
+				return new ConstTableIA(ii, gf);
+			}
 		case GET_ITEM:
 			{
 				final GetItemExpression gie = (GetItemExpression) expression;
