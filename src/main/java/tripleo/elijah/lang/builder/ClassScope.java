2aac60402f1393a51a5eb01cb14dc9702d621616
Tripleo envybox <oluoluolu+elijah@gmail.com>
2020-12-27 12:59:14 -0500

    remove docstrings from ClassScope (its it BaseScope)

--- src/main/java/tripleo/elijah/lang/builder/ClassScope.java
+++ src/main/java/tripleo/elijah/lang/builder/ClassScope.java
@@ -8,32 +8,13 @@
  */
 package tripleo.elijah.lang.builder;
 
-import antlr.Token;
-import tripleo.elijah.lang.*;
-
-import java.util.ArrayList;
-import java.util.List;
+import tripleo.elijah.lang.AccessNotation;
+import tripleo.elijah.lang.Documentable;
 
 /**
  * Created 12/22/20 8:38 PM
  */
 public class ClassScope extends ClassOrNamespaceScope implements Documentable {
-	private final List<OS_Element> _items = new ArrayList<OS_Element>();
-	private final List<Token> _docstrings = new ArrayList<Token>();
-
-//	@Override
-//	public Iterable<OS_Element> items() {
-//		return _items;
-//	}
-
-	public void add(OS_Element item) {
-		_items.add(item);
-	}
-
-	@Override
-	public void addDocString(Token s1) {
-		_docstrings.add(s1);
-	}
 
 	public void addAccess(AccessNotation acs) {
 		// TODO find something to do with this
