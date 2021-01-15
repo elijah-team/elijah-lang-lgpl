62fb540479130e7cbae4fb023454d4c6f3f1c9eb
Tripleo envybox <oluoluolu+elijah@gmail.com>
2021-01-15 07:12:50 -0500

    Make sure to set Ident Context properly

--- src/main/java/tripleo/elijah/lang/builder/TypeAliasBuilder.java
+++ src/main/java/tripleo/elijah/lang/builder/TypeAliasBuilder.java
@@ -45,6 +45,10 @@ public class TypeAliasBuilder extends ElBuilder {
 	@Override
 	public void setContext(Context context) {
 		_context = context;
+		// TODO this is a very important potential bug
+		//  where ident's may not be getting the right context
+		//  because of non-use of Parser.cur in the Builders
+		newAlias.setContext(context);
 	}
 }
 
