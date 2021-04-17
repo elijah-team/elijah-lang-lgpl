60dd3cfd690f2d844bc03816e7eb7c93c530c726
Tripleo envybox <oluoluolu+elijah@gmail.com>
2021-04-17 06:57:19 -0400

    handle null case (ie when var is tmp)

--- src/main/java/tripleo/elijah/stages/deduce/DeduceTypes2.java
+++ src/main/java/tripleo/elijah/stages/deduce/DeduceTypes2.java
@@ -422,7 +422,7 @@ public class DeduceTypes2 {
 				} else {
 					// potential_size == 0
 					// Result is handled by phase.typeDecideds, self is always valid
-					if (!(vte.getName().equals("Result") || vte.getName().equals("self")))
+					if (vte.getName() != null && !(vte.getName().equals("Result") || vte.getName().equals("self")))
 						errSink.reportDiagnostic(new CantDecideType(vte, vte.potentialTypes()));
 				}
 			}
