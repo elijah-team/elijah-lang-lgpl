6737695bf852c67fe0942184a25310082dd3fd03
Tripleo envybox <oluoluolu+elijah@gmail.com>
2021-05-31 13:56:29 -0400

    preview for GenType

---
+++ src/main/java/tripleo/elijah/stages/gen_fn/GenType.java
@@ -0,0 +1,24 @@
+/*
+ * Elijjah compiler, copyright Tripleo <oluoluolu+elijah@gmail.com>
+ *
+ * The contents of this library are released under the LGPL licence v3,
+ * the GNU Lesser General Public License text was downloaded from
+ * http://www.gnu.org/licenses/lgpl.html from `Version 3, 29 June 2007'
+ *
+ */
+package tripleo.elijah.stages.gen_fn;
+
+import tripleo.elijah.lang.OS_Type;
+
+/**
+ * Created 5/31/21 1:32 PM
+ */
+public class GenType {
+	OS_Type typeName; // TODO or just TypeName ??
+	OS_Type resolved;
+	GeneratedNode node;
+}
+
+//
+//
+//
