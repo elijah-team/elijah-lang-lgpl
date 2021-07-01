3e58e43574f78a58e63f39e5cd45eae2e4be33b7
Tripleo envybox <oluoluolu+elijah@gmail.com>
2021-07-01 01:51:10 -0400

    fix: was comparing String to OS_Package

--- src/main/java/tripleo/elijah/stages/translate/TranslateModule.java
+++ src/main/java/tripleo/elijah/stages/translate/TranslateModule.java
@@ -52,9 +52,9 @@ public class TranslateModule {
 		final TabbedOutputStream w = stream_for(item.getPackageName().getName(), cls_name);
 		try {
 			{
-				final OS_Package packageName = item.getPackageName();
+				final String packageName = item.getPackageName().getName();
 				if (!packageName.equals("")) {
-					final String pkg_decl = "package " + packageName.getName()+";";
+					final String pkg_decl = "package " + packageName+";";
 					w.put_string_ln(pkg_decl);
 					w.put_string_ln("");
 				}
@@ -90,9 +90,9 @@ public class TranslateModule {
 		final TabbedOutputStream w = stream_for(item.getPackageName().getName(), ns_name);
 		try {
 			{
-				final OS_Package packageName = item.getPackageName();
+				final String packageName = item.getPackageName().getName();
 				if (!packageName.equals("")) {
-					final String pkg_decl = "package " + packageName.getName()+";";
+					final String pkg_decl = "package " + packageName+";";
 					w.put_string_ln(pkg_decl);
 					w.put_string_ln("");
 				}
