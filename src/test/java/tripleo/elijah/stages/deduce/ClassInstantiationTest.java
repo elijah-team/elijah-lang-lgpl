5032716a6b7665737fcebd4c72f1463bc38edf85
Tripleo envybox <oluoluolu@gmail.com>
2021-07-14 05:47:34 -0400

    classInstantiation3

--- src/test/java/tripleo/elijah/stages/deduce/ClassInstantiationTest.java
+++ src/test/java/tripleo/elijah/stages/deduce/ClassInstantiationTest.java
@@ -35,6 +35,14 @@ public class ClassInstantiationTest {
 
 		c.feedCmdLine(List_of(f));
 	}
+
+	@Test
+	public void classInstantiation3() {
+		String f = "test/basic1/class_instantiation3/";
+		Compilation c = new Compilation(new StdErrSink(), new IO());
+
+		c.feedCmdLine(List_of(f));
+	}
 }
 
 //
