48add372a510a651b8b976b7a26606fed40928a5
Tripleo envybox <oluoluolu@gmail.com>
2021-07-14 06:39:53 -0400

    update comments

--- src/main/java/tripleo/elijah/stages/gen_fn/BaseGeneratedFunction.java
+++ src/main/java/tripleo/elijah/stages/gen_fn/BaseGeneratedFunction.java
@@ -100,9 +100,9 @@ public abstract class BaseGeneratedFunction extends AbstractDependencyTracker im
 	/**
 	 * Returns a string that represents the path encoded by ia2.
 	 * Does not transform the string into target language (ie C).
-	 * Called from {@link DeduceTypes2#do_assign_call(GeneratedFunction, Context, IdentTableEntry, FnCallArgs, int)}
+	 * Called from {@link DeduceTypes2#do_assign_call(BaseGeneratedFunction, Context, IdentTableEntry, FnCallArgs, int)}
 	 * or {@link DeduceTypes2#deduce_generated_function(GeneratedFunction)}
-	 * or {@link DeduceTypes2#resolveIdentIA_(Context, IdentIA, GeneratedFunction, FoundElement)}
+	 * or {@link DeduceTypes2#resolveIdentIA_(Context, IdentIA, BaseGeneratedFunction, FoundElement)}
 	 *
 	 * @param ia2 the path
 	 * @return a string that represents the path encoded by ia2
