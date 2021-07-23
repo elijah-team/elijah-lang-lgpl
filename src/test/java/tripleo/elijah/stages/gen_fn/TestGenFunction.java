ba986bc196f83f2eb902db75d94e76499b8d9918
Tripleo envybox <oluoluolu@gmail.com>
2021-07-22 20:48:55 -0400

    make tester happy

--- src/test/java/tripleo/elijah/stages/gen_fn/TestGenFunction.java
+++ src/test/java/tripleo/elijah/stages/gen_fn/TestGenFunction.java
@@ -199,7 +199,7 @@ public class TestGenFunction {
 		dp.finish();
 
 		Assert.assertEquals("Not all hooks ran", 4, ran_hooks.size());
-		Assert.assertEquals(12, c.errorCount());
+		Assert.assertEquals(13, c.errorCount());
 	}
 
 	@Test
