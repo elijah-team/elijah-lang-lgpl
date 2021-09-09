5682a0d4f7be252ff046a19a484ce74149031c98
Tripleo envybox <oluoluolu@gmail.com>
2021-09-09 06:59:55 -0400

    ResolveUnknown

---
+++ src/main/java/tripleo/elijah/stages/deduce/ResolveUnknown.java
@@ -0,0 +1,59 @@
+/*
+ * Elijjah compiler, copyright Tripleo <oluoluolu+elijah@gmail.com>
+ *
+ * The contents of this library are released under the LGPL licence v3,
+ * the GNU Lesser General Public License text was downloaded from
+ * http://www.gnu.org/licenses/lgpl.html from `Version 3, 29 June 2007'
+ *
+ */
+package tripleo.elijah.stages.deduce;
+
+import org.jetbrains.annotations.NotNull;
+import tripleo.elijah.diagnostic.Diagnostic;
+import tripleo.elijah.diagnostic.Locatable;
+
+import java.io.PrintStream;
+import java.util.List;
+
+/**
+ * Created 9/9/21 6:25 AM
+ */
+public class ResolveUnknown implements Diagnostic {
+	@Override
+	public String code() {
+		return "E1003";
+	}
+
+	@Override
+	public Severity severity() {
+		return Severity.ERROR;
+	}
+
+	@Override
+	public @NotNull Locatable primary() {
+		return null;
+	}
+
+	@Override
+	public @NotNull List<Locatable> secondary() {
+		return null;
+	}
+
+	@Override
+	public void report(PrintStream stream) {
+		stream.println(String.format("---[%s]---: %s", code(), message()));
+		// linecache.print(primary);
+		for (Locatable sec : secondary()) {
+			//linecache.print(sec)
+		}
+		stream.flush();
+	}
+
+	private String message() {
+		return "Can't resolve variable";
+	}
+}
+
+//
+//
+//
