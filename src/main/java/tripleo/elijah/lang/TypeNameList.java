53a536c4a9726356a521842a429780520b78c1ce
Tripleo envybox <oluoluolu@gmail.com>
2021-09-17 23:48:57 -0400

    set vim and emacs modes

--- src/main/java/tripleo/elijah/lang/TypeNameList.java
+++ src/main/java/tripleo/elijah/lang/TypeNameList.java
@@ -1,3 +1,4 @@
+/* -*- Mode: Java; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
 /*
  * Elijjah compiler, copyright Tripleo <oluoluolu+elijah@gmail.com>
  * 
@@ -44,5 +45,5 @@ public class TypeNameList {
 }
 
 //
-//
+// vim:set shiftwidth=4 softtabstop=0 noexpandtab:
 //
