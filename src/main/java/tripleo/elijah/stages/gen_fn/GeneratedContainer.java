a894a5bc132cd8c822a440c7a9bd4e9d10d689c5
Tripleo envybox <oluoluolu@gmail.com>
2021-11-05 00:44:50 -0400

    tabs instead of spaces

--- src/main/java/tripleo/elijah/stages/gen_fn/GeneratedContainer.java
+++ src/main/java/tripleo/elijah/stages/gen_fn/GeneratedContainer.java
@@ -21,65 +21,65 @@ import java.util.List;
  * Created 2/28/21 3:23 AM
  */
 public interface GeneratedContainer extends GeneratedNode {
-    OS_Element getElement();
+	OS_Element getElement();
 
-    VarTableEntry getVariable(String aVarName);
+	VarTableEntry getVariable(String aVarName);
 
-    public class VarTableEntry {
-        public final VariableStatement vs;
-        public final IdentExpression nameToken;
-        public final IExpression initialValue;
-        private final OS_Element parent;
-        TypeName typeName;
-        public OS_Type varType;
-        List<TypeTableEntry> potentialTypes = new ArrayList<TypeTableEntry>();
-        private GeneratedNode _resolvedType;
+	public class VarTableEntry {
+		public final VariableStatement vs;
+		public final IdentExpression nameToken;
+		public final IExpression initialValue;
+		private final OS_Element parent;
+		TypeName typeName;
+		public OS_Type varType;
+		List<TypeTableEntry> potentialTypes = new ArrayList<TypeTableEntry>();
+		private GeneratedNode _resolvedType;
 
-        public VarTableEntry(final VariableStatement aVs,
-                             final @NotNull IdentExpression aNameToken,
-                             final IExpression aInitialValue,
-                             final @NotNull TypeName aTypeName,
-                             final @NotNull OS_Element aElement) {
-            vs              = aVs;
-            nameToken       = aNameToken;
-            initialValue    = aInitialValue;
-            typeName        = aTypeName;
-            varType         = new OS_Type(typeName);
-            parent          = aElement;
-        }
+		public VarTableEntry(final VariableStatement aVs,
+							 final @NotNull IdentExpression aNameToken,
+							 final IExpression aInitialValue,
+							 final @NotNull TypeName aTypeName,
+							 final @NotNull OS_Element aElement) {
+			vs              = aVs;
+			nameToken       = aNameToken;
+			initialValue    = aInitialValue;
+			typeName        = aTypeName;
+			varType         = new OS_Type(typeName);
+			parent          = aElement;
+		}
 
-        public void addPotentialTypes(@NotNull Collection<TypeTableEntry> aPotentialTypes) {
-            potentialTypes.addAll(aPotentialTypes);
-        }
+		public void addPotentialTypes(@NotNull Collection<TypeTableEntry> aPotentialTypes) {
+			potentialTypes.addAll(aPotentialTypes);
+		}
 
-        public void resolve(@NotNull GeneratedNode aResolvedType) {
-            _resolvedType = aResolvedType;
-        }
+		public void resolve(@NotNull GeneratedNode aResolvedType) {
+			_resolvedType = aResolvedType;
+		}
 
-        public @Nullable GeneratedNode resolvedType() {
-            return _resolvedType;
-        }
+		public @Nullable GeneratedNode resolvedType() {
+			return _resolvedType;
+		}
 
-        public @NotNull OS_Element getParent() {
-            return parent;
-        }
+		public @NotNull OS_Element getParent() {
+			return parent;
+		}
 
-        public void connect(final VariableTableEntry aVte, final GeneratedConstructor aConstructor) {
-            connectionPairs.add(new ConnectionPair(aVte, aConstructor));
-        }
+		public void connect(final VariableTableEntry aVte, final GeneratedConstructor aConstructor) {
+			connectionPairs.add(new ConnectionPair(aVte, aConstructor));
+		}
 
-        public List<ConnectionPair> connectionPairs = new ArrayList<>();
+		public List<ConnectionPair> connectionPairs = new ArrayList<>();
 
-        public static class ConnectionPair {
-            public final VariableTableEntry vte;
-            final GeneratedConstructor constructor;
+		public static class ConnectionPair {
+			public final VariableTableEntry vte;
+			final GeneratedConstructor constructor;
 
-            public ConnectionPair(final VariableTableEntry aVte, final GeneratedConstructor aConstructor) {
-                vte = aVte;
-                constructor = aConstructor;
-            }
-        }
-    }
+			public ConnectionPair(final VariableTableEntry aVte, final GeneratedConstructor aConstructor) {
+				vte = aVte;
+				constructor = aConstructor;
+			}
+		}
+	}
 }
 
 //
