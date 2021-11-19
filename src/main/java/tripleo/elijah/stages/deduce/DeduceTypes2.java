88e9a3dfc1ec593867eb11d11ecbd3c1d7293b60
Tripleo envybox <oluoluolu@gmail.com>
2021-11-19 17:25:18 -0500

    added missing cases
    * No implementation

--- src/main/java/tripleo/elijah/stages/deduce/DeduceTypes2.java
+++ src/main/java/tripleo/elijah/stages/deduce/DeduceTypes2.java
@@ -2394,7 +2394,10 @@ public class DeduceTypes2 {
 					}
 				}
 				break;
-
+			case ADDITION: case MODULO: case SUBTRACTION:
+				int y=2;
+				System.err.println("2363");
+				break;
 			case GET_ITEM:
 				{
 					final @NotNull GetItemExpression gie = (GetItemExpression) e;
