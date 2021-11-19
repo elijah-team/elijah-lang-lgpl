1d79ec53e1fa4261eeb31f808cf850aa5b4ad643
Tripleo envybox <oluoluolu@gmail.com>
2021-11-18 22:10:14 -0500

    imports

--- src/main/java/tripleo/elijah/stages/gen_fn/IdentTableEntry.java
+++ src/main/java/tripleo/elijah/stages/gen_fn/IdentTableEntry.java
@@ -11,6 +11,7 @@ package tripleo.elijah.stages.gen_fn;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.jdeferred2.DoneCallback;
+import org.jdeferred2.Promise;
 import org.jdeferred2.impl.DeferredObject;
 import org.jetbrains.annotations.NotNull;
 import tripleo.elijah.lang.Context;
