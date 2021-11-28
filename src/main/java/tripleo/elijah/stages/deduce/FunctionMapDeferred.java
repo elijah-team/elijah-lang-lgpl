2a5873e4ae44d96778855c66e8847ed5f9ddf1e0
Tripleo envybox <oluoluolu@gmail.com>
2021-11-28 11:56:10 -0500

    feature: Introduce FunctionMapDeferred (oopsie)

---
+++ src/main/java/tripleo/elijah/stages/deduce/FunctionMapDeferred.java
@@ -0,0 +1,23 @@
+/* -*- Mode: Java; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/*
+ * Elijjah compiler, copyright Tripleo <oluoluolu+elijah@gmail.com>
+ *
+ * The contents of this library are released under the LGPL licence v3,
+ * the GNU Lesser General Public License text was downloaded from
+ * http://www.gnu.org/licenses/lgpl.html from `Version 3, 29 June 2007'
+ *
+ */
+package tripleo.elijah.stages.deduce;
+
+import tripleo.elijah.stages.gen_fn.GeneratedFunction;
+
+/**
+ * Created 11/27/21 3:31 PM
+ */
+public interface FunctionMapDeferred {
+	void onNotify(GeneratedFunction aGeneratedFunction);
+}
+
+//
+// vim:set shiftwidth=4 softtabstop=0 noexpandtab:
+//
