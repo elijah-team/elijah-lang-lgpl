730ff78c00a77ad7d1d9168ff34035461a8e678d
Tripleo envybox <oluoluolu@gmail.com>
2021-11-29 01:07:48 -0500

    add ContextInfo (oops)

---
+++ src/main/java/tripleo/elijah/contexts/ContextInfo.java
@@ -0,0 +1,21 @@
+/* -*- Mode: Java; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/*
+ * Elijjah compiler, copyright Tripleo <oluoluolu+elijah@gmail.com>
+ *
+ * The contents of this library are released under the LGPL licence v3,
+ * the GNU Lesser General Public License text was downloaded from
+ * http://www.gnu.org/licenses/lgpl.html from `Version 3, 29 June 2007'
+ *
+ */
+
+package tripleo.elijah.contexts;
+
+/**
+ * Created 11/29/21 12:26 AM
+ */
+public interface ContextInfo {
+}
+
+//
+// vim:set shiftwidth=4 softtabstop=0 noexpandtab:
+//
