8710c56e2fe124640ec779b4f7334294fa24271d
Tripleo Nova <oluoluolu+elijah@gmail.com>
2022-05-07 22:58:22 -0400

    [xx] #registerPipelineLogic with jdeferred
    * Doesn't see any use
      - completely forgot about this, which is good - we can delete it soon
    * Could have just as easily used java.util.function.Consumer
      - might actually be better...

--- src/main/java/tripleo/elijah/comp/DefaultCompilationAccess.java
+++ src/main/java/tripleo/elijah/comp/DefaultCompilationAccess.java
@@ -1,18 +1,37 @@
 package tripleo.elijah.comp;
 
+import org.jdeferred2.DoneCallback;
 import org.jetbrains.annotations.NotNull;
+import tripleo.elijah.stages.gen_fn.DeferredObject2;
 import tripleo.elijah.stages.logging.ElLog;
+import io.reactivex.rxjava3.functions.Consumer;
 
 class DefaultCompilationAccess implements ICompilationAccess {
 	private final Compilation compilation;
+	private         DeferredObject2<PipelineLogic, Void, Void> pipelineLogicDeferred = new DeferredObject2<>();
 
 	public DefaultCompilationAccess(final Compilation aCompilation) {
 		compilation = aCompilation;
 	}
 
+	void registerPipelineLogic(final Consumer<PipelineLogic> aPipelineLogicConsumer) {
+		pipelineLogicDeferred.then(new DoneCallback<PipelineLogic>() {
+			@Override
+			public void onDone(final PipelineLogic result) {
+				try {
+					aPipelineLogicConsumer.accept(result);
+				} catch (Throwable aE) {
+					throw new RuntimeException(aE);
+				}
+			}
+		});
+	}
+
 	@Override
 	public void setPipelineLogic(final PipelineLogic pl) {
 		compilation.pipelineLogic = pl;
+
+		pipelineLogicDeferred.resolve(pl);
 	}
 
 	@Override
